0a82572d2036d0e18d6c3ab4b830f6651a794ec1
Casey Carroll <caseydean8@gmail.com>
2023-02-08 14:46:43 -0500

    Added execute methods

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+// attribute ApiController
+[ApiController]
+// "[controller]" looks for the word before Controller in the clas name, User in this instance
+[Route("[controller]")]
+// class Microsoft.AspNetCore.Mvc.ControllerBase
+// A base class for an MVC controller without view support\.
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        // Console.WriteLine(config.GetConnectionString("DefaultConnection"));
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+
+      public DateTime TestConnection()
+      {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+      }
+    // [HttpGet]
+    [HttpGet("GetUsers/{testValue}")]
+    // public IEnumerable<User> GetUsers()
+
+    public string[] GetUsers(string testValue)
+    {
+        return new string[] { "user1", "user2", testValue };
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+// WebApplicationBuilder WebApplication.CreateBuilder(string[] args) (+ 2 overloads)
+// Initializes a new instance of the `WebApplicationBuilder` class with preconfigured defaults\.
+// Returns: The `WebApplicationBuilder`\.
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((options) =>
+    {
+        options.AddPolicy("DevCors", (corsBuilder) =>
+            {
+                // default ports for angular, react, vue
+                corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8080")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCors");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
