7c8998cf3489c1fc7a9514aedd878791130acd7b
Casey Carroll <caseydean8@gmail.com>
2023-02-08 19:56:06 -0500

    Added Models

---
+++ Models/UserJobInfo.cs
@@ -0,0 +1,24 @@
+namespace DotnetAPI
+{
+    // it's good practice to make Models partial in case you need to add to them later from other files
+    public partial class UserJobInfo
+    {
+        public int UserId { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+
+        public UserJobInfo()
+        {
+            if (JobTitle  == null)
+            {
+                JobTitle = "";
+            }
+            if (Department == null)
+            {
+                Department = "";
+            }
+        }
+    }
+
+}
+
---
+++ Models/UserSalary.cs
@@ -0,0 +1,15 @@
+
+namespace DotnetAPI
+{
+    // it's good practice to make Models partial in case you need to add to them later from other files
+    public partial class UserSalary
+    {
+        public int UserId { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public decimal AvgSalary { get; set; }
+
+    }
+
+}
