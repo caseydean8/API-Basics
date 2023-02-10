7cf6c886de23da80a4598b893e0a6bfe08394dac
Casey Carroll <caseydean8@gmail.com>
2023-02-10 14:54:06 -0500

    Added dto for AddUser route

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DotnetAPI.Controllers;
 // attribute ApiController
 [ApiController]
-// "[controller]" looks for the word before Controller in the clas name, User in this instance
+// "[controller]" looks for the word before Controller in the class name, User in this instance
 [Route("[controller]")]
 // class Microsoft.AspNetCore.Mvc.ControllerBase
 // A base class for an MVC controller without view support\.
@@ -23,9 +23,8 @@ public class UserController : ControllerBase
     {
         return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
     }
-    // [HttpGet]
+
     [HttpGet("GetUsers")]
-    // public IEnumerable<User> GetUsers()
 
     public IEnumerable<User> GetUsers()
     {
@@ -49,6 +48,7 @@ public class UserController : ControllerBase
         // .ToArray();
     }
 
+
     [HttpGet("GetSingleUser/{userId}")]
     // public IEnumerable<User> GetUser()
 
@@ -69,13 +69,14 @@ public class UserController : ControllerBase
         return user;
     }
 
+
     [HttpPut("EditUser")]
 
     // interface Microsoft.AspNetCore.Mvc.IActionResult
     // Defines a contract that represents the result of an action method\.
     // class Microsoft.AspNetCore.Mvc.FromBodyAttribute (+ 1 overload)
-    // Specifies that a parameter or property should be bound using the request body\.
-    // public IActionResult EditUser([FromBody]) // taken from the payload/ req.body
+    // public IActionResult EditUser([FromBody]) // FromBody: taken from the req.body payload.
+    // IActionResult UserController.EditUser(DotnetAPI.User user) // User from DotnetAPI/Models/Users.cs
     public IActionResult EditUser(User user)
     {
         string sql = @"
@@ -98,9 +99,10 @@ public class UserController : ControllerBase
         throw new Exception("Failed to Update User");
     }
 
+
     [HttpPost("AddUser")]
 
-    public IActionResult AddUser(User user)
+    public IActionResult AddUser(UserDto user)
     {
         string sql = @"
         INSERT INTO TutorialAppSchema.Users
@@ -118,7 +120,7 @@ public class UserController : ControllerBase
              "', '" + user.Gender +
              "', '" + user.Active +
             "')";
-        // "' WHERE UserId = " + user.UserId;
+
         Console.WriteLine(sql);
         if (_dapper.ExecuteSql(sql))
         {
---
+++ Dtos/UserDto.cs
@@ -0,0 +1,34 @@
+namespace DotnetAPI
+{
+    // it's good practice to make Models partial in case you need to add to them later from other files
+    // Dto: data transfer object
+    public partial class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public bool Active { get; set; }
+
+        public UserDto()
+        {
+            if (FirstName == null)
+            {
+                FirstName = "";
+            }
+            if (LastName == null)
+            {
+                LastName = "";
+            }
+            if (Email == null)
+            {
+                Email = "";
+            }
+            if (Gender == null)
+            {
+                Gender = "";
+            }
+        }
+    }
+
+}
