e2d2edaa40c223c2ab12e60b9d927aff60a6a0fc
Casey Carroll <caseydean8@gmail.com>
2023-02-24 14:59:07 -0500

    Added parameter to get User by Id

--- SqlScripts/Stored_Procedure_Creation.sql
+++ SqlScripts/Stored_Procedure_Creation.sql
@@ -14,7 +14,11 @@ GO
 -- CREATE PROCEDURE TutorialAppSchema.spUsers_Get -- CREATE PROCEDURE can only be run once for procedure name.
 -- To change PROCEDURE use "ALTER"
 ALTER PROCEDURE TutorialAppSchema.spUsers_Get
--- EXEC TutorialAppSchema.spUsers_Get -- used to call procedure
+    -- EXEC TutorialAppSchema.spUsers_Get -- used to call procedure
+    -- EXEC TutorialAppSchema.spUsers_Get 3 -- works but becomes confusing with multiple variables
+    -- EXEC TutorialAppSchema.spUsers_Get @UserId=3 -- same as above but more clear
+    -- @ creates a variable in Sql. Make sure to declare type or sql will implicitly convert and slow down query.
+    @UserId INT
 AS
 BEGIN
     SELECT [Users].[UserId],
@@ -24,6 +28,7 @@ BEGIN
         [Users].[Gender],
         [Users].[Active]
     FROM TutorialAppSchema.Users AS Users
+    WHERE Users.UserId = @UserId
 END
 
 -- Don't put anything after END, it will be picked up by stored procedure. 
