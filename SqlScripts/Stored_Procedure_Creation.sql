f9e1622efa237358c0542163d6022fcd2d442087
Casey Carroll <caseydean8@gmail.com>
2023-02-24 19:31:32 -0500

    Join and Outer Apply Salary and JobInfo tables

--- SqlScripts/Stored_Procedure_Creation.sql
+++ SqlScripts/Stored_Procedure_Creation.sql
@@ -21,13 +21,40 @@ ALTER PROCEDURE TutorialAppSchema.spUsers_Get
     @UserId INT = NULL
 AS
 BEGIN
+    -- SELECT UserJobInfo.Department
+    --     , AVG(UserSalary.Salary)
+    -- FROM TutorialAppSchema.Users AS Users
+    --     LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+    --     ON UserSalary.UserId = Users.UserId
+    --     LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    --     ON UserJobInfo.UserId = Users.UserId
+    -- GROUP BY UserJobInfo.Department
+
     SELECT [Users].[UserId],
         [Users].[FirstName],
         [Users].[LastName],
         [Users].[Email],
         [Users].[Gender],
-        [Users].[Active]
+        [Users].[Active],
+        UserSalary.Salary,
+        UserJobInfo.Department,
+        UserJobInfo.JobTitle,
+        AvgSalary.AvgSalary
     FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+        OUTER APPLY (
+            SELECT AVG(UserSalary2.Salary) AvgSalary
+        FROM TutorialAppSchema.Users AS Users
+            LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary2
+            ON UserSalary2.UserId = Users.UserId
+            LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+            ON UserJobInfo2.UserId = Users.UserId
+            WHERE UserJobInfo2.Department = UserJobInfo.Department
+        GROUP BY UserJobInfo2.Department
+        ) AS AvgSalary
     WHERE Users.UserId = ISNULL (@UserId, Users.UserId)
 END
 
